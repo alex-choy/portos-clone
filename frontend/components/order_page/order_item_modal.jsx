279400fdc10af1a4277d64a55ac89de0b354d5e3
Alex Choy <alexchoy2345@gmail.com>
2020-08-22 18:13:42 -0700

    added quantity buttons to order page modal

--- frontend/components/order_page/order_item_modal.jsx
+++ frontend/components/order_page/order_item_modal.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
-import FoodIndexItem from '../foods/food_index_item';
+import React from "react";
+import OrderItemQuantity from './order_page_quantity';
 
 class OrderItemModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       quantity: 1,
-      price: this.props.foodItem.price
+      price: this.props.foodItem.price,
+      decreaseAllowed: true,
     };
+    this.increaseQuantity = this.increaseQuantity.bind(this);
+    this.decreaseQuantity = this.decreaseQuantity.bind(this);
   }
 
+  increaseQuantity() {
+    this.setState({ quantity: this.state.quantity + 1, decreaseAllowed: true });
+  }
+
+  decreaseQuantity() {
+    const currQuantity = this.state.quantity;
+    if (currQuantity > 1) {
+      this.setState({ quantity: currQuantity - 1 });
+    }
+  }
 
   render() {
     const { name, description, photo_url } = this.props.foodItem;
+    const { quantity } = this.state;
     return (
       <div className="order-item-modal-wrapper">
+        <h2>{name}</h2>
         <section className="order-modal-food-item">
-          <h2>{name}</h2>
           <span className="order-modal-details">
             <p>{description}</p>
-            <img src={photo_url} alt=""/>
+            <OrderItemQuantity
+              quantity={quantity}
+              increaseQuantity={this.increaseQuantity}
+              decreaseQuantity={this.decreaseQuantity}
+            />
           </span>
-        </section>
-        <section className="order-modal-quantitiy">
-          <span>
-            quantity thing
-            total price
-          </span>
+          <img src={photo_url} alt="" />
         </section>
       </div>
     );
