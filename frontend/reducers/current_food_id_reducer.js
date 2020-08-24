52e0731af7cfca69fb73594a79c577b992308f86
Alex Choy <alexchoy2345@gmail.com>
2020-08-24 11:28:47 -0700

    fixed previous error. currentFoodId state is null by default, so the oldState is returned to stay on the same modal after adding items to cart

--- frontend/reducers/current_food_id_reducer.js
+++ frontend/reducers/current_food_id_reducer.js
@@ -1,10 +1,13 @@
 import { SET_ORDER_ITEM_MODAL_ID } from '../actions/modal_actions';
+import { ADD_ITEM_TO_CART } from '../actions/shopping_cart_actions';
 
 const currentFoodIdReducer = (oldState, action) => {
   Object.freeze(oldState);
   switch(action.type) {
     case SET_ORDER_ITEM_MODAL_ID:
       return action.foodId
+    case ADD_ITEM_TO_CART:
+      return oldState;
     default:
       return null;
   }
