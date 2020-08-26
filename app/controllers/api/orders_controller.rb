9b8f2df646acf0cd3500bfc6e3af055ea5b6d6e7
Alex Choy <alexchoy2345@gmail.com>
2020-08-25 22:10:32 -0700

    can create orders, orderedFoodItems, and update quantities in the backend

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -21,14 +21,31 @@ class Api::OrdersController < ApplicationController
       end
     end
 
-    order = Order.new(order_params)
+    @order = Order.new(order_params)
 
-    if order.save 
+    if @order.save 
       shopping_cart.each do |food_info|
-        
+        food_id = food_info["foodId"]
+        quantity = food_info["quantity"]
+        ofi = OrderedFoodItem.new(
+          order_id: @order.id, 
+          food_item_id: food_id, 
+          quantity: quantity)
+
+        if ofi.save
+          food_item = FoodItem.find(food_id)
+          food_item.quantity -= quantity
+          if food_item.save
+          else
+            render json: food_item.errors.full_messages, status: 422
+          end
+        else
+          render json: ofi.errors.full_messages, status: 422
+        end
       end
+      render json: ["Successfully created all food items!"]
     else
-      render json: order.errors.full_messages, status: 422
+      render json: @order.errors.full_messages, status: 422
     end
 
   end
