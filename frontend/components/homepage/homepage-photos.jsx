cdeb0477643fc5b795e8e006b83dd3299fdefd78
Alex Choy <alexchoy2345@gmail.com>
2020-09-22 12:15:41 -0700

    some button hover changes

--- frontend/components/homepage/homepage-photos.jsx
+++ frontend/components/homepage/homepage-photos.jsx
@@ -6,20 +6,22 @@ const HomepagePhotos = () => {
   return (
     <section className="homepage-photos cf">
       <section className="homepage-photo-wrapper">
-        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/ddp-QnQSxrXFgnw-unsplash.jpg" alt="Country Bread" />
+        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/stephen-walker-_-SwhhV7tSo-unsplash.jpg" />
         <section className="photo-info">
-          <div className="photo-text">
-            Hey What's Up Hello
-            <Link to="/menu/20"></Link>
+          <div>
+            <span className="photo-text">Artisan Sourdough Bread</span>
+            <br />
+            <Link className="photo-btn" to="/menu/7">Check it out!</Link>
           </div>
         </section>
       </section>
       <section className="homepage-photo-wrapper">
-        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/corleto-peanut-butter-HIXCrSevIKQ-unsplash.jpg" alt="Cookies" />
+        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/american-heritage-chocolate-DoK5qEy2L60-unsplash.jpg" alt="Cookies" />
         <section className="photo-info">
-          <div className="photo-text">
-            Hey What's Up Hello
-            <Link to="/menu/20"></Link>
+          <div>
+            <span className="photo-text">Chocolate Cookies</span>
+            <br/>
+            <Link className="photo-btn" to="/menu/2">Check it out!</Link>
           </div>
         </section>
       </section>
