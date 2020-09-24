003a3da82210cb3b70ad867203cfed5a39f7ddc8
Alex Choy <alexchoy2345@gmail.com>
2020-09-24 08:40:32 -0700

    search works, need to style

--- frontend/components/foods/food_index.jsx
+++ frontend/components/foods/food_index.jsx
@@ -2,23 +2,57 @@ import React from 'react';
 import FoodIndexItem from './food_index_item';
 
 class FoodIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredFoodItems: props.foodItems,
+      userInput: ""
+    }
+    this.changeInput = this.changeInput.bind(this);
+  }
   componentDidMount() {
-    // get all food items from DB
     this.props.getFoodItems();
   }
 
+  changeInput() {
+    event.preventDefault();
+    this.setState({
+      userInput: event.target.value,
+      filteredFoodItems: this.props.foodItems.filter(foodItem => {
+        return foodItem.name.toLowerCase().includes(event.target.value);
+      })
+    })
+  }
+
   createFoodItems() {
-    return this.props.foodItems.map((foodItem) => (
+    let currFoodItems;
+    if(this.state.userInput === "") {
+      currFoodItems = this.props.foodItems;
+    } else {
+      currFoodItems = this.state.filteredFoodItems;
+    }
+    if(currFoodItems.length < 1) {
+      return <div><span>No results</span></div>
+    }
+    return currFoodItems.map((foodItem) => (
       <FoodIndexItem key={foodItem.id} foodItem={foodItem}/>
     ));
   }
 
-  render() {
-    return (
-      <div className="food-card-list cf">
-        {this.createFoodItems()}
-      </div>
-    );
+  render() { 
+    if(this.props.foodItems.length > 0) {
+      return (
+        <div className="food-card-list cf">
+
+          <section className="search-bar">
+            <i className="fa fa-search" aria-hidden="true"></i>
+            <input type="text" className="search-food" placeholder="Search..." onChange={this.changeInput}/>
+          </section>
+          {this.createFoodItems()}
+        </div>
+      );
+    } 
+    return <div><span>Loading...</span></div>
   }
 }
 
--- frontend/components/foods/menu.jsx
+++ frontend/components/foods/menu.jsx
@@ -3,18 +3,12 @@ import FoodIndexContainer from './food_index_container';
 import MenuAside from './menu_aside';
 
 class Menu extends React.Component {
-
   render() {
     return (
       <section className="avoid-masthead">
-        {/* picture, 270px height, 100% width */}
-        {/* 20-25% left nav-bar for sorting? */}
-        {/* 75-80% right menu grid */}
-        {/* <aside className="col col-1-5">
-          <MenuAside />
-        </aside> */}
         <section className="food-index-wrapper">
-          <FoodIndexContainer />
+          <h1 className="page-header">MENU</h1>
+            <FoodIndexContainer />
         </section>
       </section>
     );
