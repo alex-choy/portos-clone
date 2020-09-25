6e3d8c53389f74f96f52f2028857696fb79efede
Alex Choy <alexchoy2345@gmail.com>
2020-09-25 11:01:14 -0700

    adding links to about in masthead

--- frontend/components/homepage/homepage-photos.jsx
+++ frontend/components/homepage/homepage-photos.jsx
@@ -7,7 +7,7 @@ const HomepagePhotos = () => {
     <section className="homepage-photos cf">
       <section className="homepage-photo-wrapper">
         <Link className="photo-btn" to="/menu/7">Check it out!</Link>
-        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/stephen-walker-_-SwhhV7tSo-unsplash.jpg" />
+        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/sourdough_bread.jpg" />
         <section className="photo-info">
           <div>
             <span className="photo-text">Artisan Sourdough Bread</span>
@@ -16,7 +16,7 @@ const HomepagePhotos = () => {
       </section>
       <section className="homepage-photo-wrapper">
         <Link className="photo-btn" to="/menu/2">Check it out!</Link>
-        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/american-heritage-chocolate-DoK5qEy2L60-unsplash.jpg" alt="Cookies" />
+        <img className="homepage-photo" src="https://porosnax-dev.s3-us-west-1.amazonaws.com/food_items/chocolate_cookies.jpg" alt="Cookies" />
         <section className="photo-info">
           <div>
             <span className="photo-text">Chocolate Cookies</span>
