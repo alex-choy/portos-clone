0325f43c73411c963895d27fb51476099776ad71
Alex Choy <alexchoy2345@gmail.com>
2020-09-25 12:22:52 -0700

    order page styling and functionality

--- frontend/components/order_page/order_page.jsx
+++ frontend/components/order_page/order_page.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import OrderPageItem from "./order_page_item";
-import Modal from '../shared_components/popup_modal';
-import ShoppingCart from './shopping_cart/shopping_cart';
+import Modal from "../shared_components/popup_modal";
+import ShoppingCart from "./shopping_cart/shopping_cart";
 
 export const SHOPPING_CART = "shoppingCart";
 
@@ -10,7 +10,7 @@ class OrderPage extends React.Component {
     super(props);
     this.state = {
       userInput: "",
-    }
+    };
 
     this.itemClick = this.itemClick.bind(this);
     // this.addItemToCart = this.addItemToCart.bind(this);
@@ -20,10 +20,11 @@ class OrderPage extends React.Component {
   }
 
   componentDidMount() {
-    let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+    let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
     this.props.getLocalShoppingCart(shoppingCart);
-    this.props.getCategoriesAndFoodItems()
-      .then(() => this.setState({filteredCategories: this.props.categories}));
+    this.props
+      .getCategoriesAndFoodItems()
+      .then(() => this.setState({ filteredCategories: this.props.categories }));
   }
 
   itemClick(foodId) {
@@ -32,40 +33,56 @@ class OrderPage extends React.Component {
     this.props.setOrderItemModalId(foodId);
   }
 
-  toTitleCase(phrase) {
-    return phrase
+  toTitleCasePlural(phrase) {
+    let titleCased = phrase
       .toLowerCase()
-      .split('_')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
-  };
-
-  changeInput(e) {
-    this.setState({userInput: e.target.value.toLowerCase()});
-    const { foodItems } = this.props;  
-    const newFilteredCats = {...this.props.categories};
-    console.log('newFiltCat: ', newFilteredCats);
-    for(const category in newFilteredCats) {
-      newFilteredCats[category] = newFilteredCats[category].filter(foodId => {
-        const foodItem = foodItems[foodId];
-        return foodItem.name.toLowerCase().includes(e.target.value.toLowerCase());
-      }); 
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+    if (titleCased.charAt(titleCased.length - 1) === "y") {
+      titleCased = titleCased.slice(0, titleCased.length - 1) + "ie";
     }
-    console.log(newFilteredCats);
-    this.setState({filteredCategories: newFilteredCats});
+    return titleCased + "s";
   }
 
+  changeInput(e) {
+    this.setState({ userInput: e.target.value.toLowerCase() }, () => {
+      const { userInput } = this.state;
+      const { foodItems } = this.props;
+      const newFilteredCats = { ...this.props.categories };
+      for (const category in newFilteredCats) {
+        const foodCategory = this.toTitleCasePlural(category).toLowerCase();
+        if (foodCategory.includes(userInput)) continue;
+
+        newFilteredCats[category] = newFilteredCats[category].filter(
+          (foodId) => {
+            const foodName = foodItems[foodId].name.toLowerCase();
+
+            return foodName.includes(userInput);
+          }
+        );
+      }
+      console.log(newFilteredCats);
+      this.setState({ filteredCategories: newFilteredCats });
+    });
+  }
 
   /**
    * Given foodIds that belong to a category, make items for each foodId
-   * @param {Number Array} foodIds 
+   * @param {Number Array} foodIds
    */
   renderEachCategory(foodIds) {
     const { foodItems } = this.props;
     let categoryEles = [];
     foodIds.forEach((id) => {
       const foodItem = foodItems[id];
-      categoryEles.push(<OrderPageItem key={id} foodItem={foodItem} itemClick={this.itemClick} />);
+      categoryEles.push(
+        <OrderPageItem
+          key={id}
+          foodItem={foodItem}
+          itemClick={this.itemClick}
+        />
+      );
     });
     return categoryEles;
   }
@@ -80,14 +97,12 @@ class OrderPage extends React.Component {
       const categoryEles = this.renderEachCategory(foodIds);
       if (categoryEles.length) {
         let categoryName = category;
-        const categoryNameLen = categoryName.length;
-        if(categoryName.charAt(categoryNameLen - 1) === "y") {
-          categoryName = categoryName.slice(0, categoryNameLen - 1) + "ie";
-        }
         categories.push(
           <div className={`category ${category} cf`} key={category}>
-            <h2 className="category-title">{this.toTitleCase(categoryName)}s</h2>
-            <hr/>
+            <h2 className="category-title">
+              {this.toTitleCasePlural(categoryName)}
+            </h2>
+            <hr />
             <section className="category-items-wrapper">{categoryEles}</section>
           </div>
         );
@@ -100,12 +115,21 @@ class OrderPage extends React.Component {
   render() {
     if (this.props.foodItems && this.state.filteredCategories) {
       return (
-        <div className="order-page">  
+        <div className="order-page">
           <h1 className="page-header">ORDER PAGE</h1>
           <div className="order-page-wrapper">
             <section className="search-food order-search-food">
-              <i id="order-fa-search" className="fa fa-search" aria-hidden="true"></i>
-              <input type="text" className="search-bar" placeholder="Search..." onChange={this.changeInput} />
+              <i
+                id="order-fa-search"
+                className="fa fa-search"
+                aria-hidden="true"
+              ></i>
+              <input
+                type="text"
+                className="search-bar"
+                placeholder="Search..."
+                onChange={this.changeInput}
+              />
             </section>
             <section className="order-items">
               {this.renderAllItemsByCategory()}
